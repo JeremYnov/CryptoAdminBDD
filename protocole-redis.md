13fc742288b7a600cc7390ef6ad196b562b56981
loux22 <louis.ardilly@yahoo.fr>
2022-01-07 10:59:10 +0100

    [add] add test and conf redis (bdd key -> value) for storage token

---
+++ protocole-redis.md
@@ -0,0 +1,7 @@
+Creation environnement virtuel
+
+pip install -r requirements.txt
+
+docker-compose up -d
+
+python test_redis.py
---
+++ test_redis.py
@@ -0,0 +1,21 @@
+
+import redis
+
+# connect to redis
+client = redis.Redis(host='localhost', port=6379)
+
+def create_key_value(key, value):
+    # set a key
+    client.set(key, value)
+
+def get_value_by_key(key):
+    # get a value
+    value = client.get(key)
+    # transform type value bytes -> str
+    return value.decode("utf-8")
+
+
+if __name__ == "__main__":
+    create_key_value("test", "heyyyyyyyyyy")
+    value = get_value_by_key("test")
+    print(value)
